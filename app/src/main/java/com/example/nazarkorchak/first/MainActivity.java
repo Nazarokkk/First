22690037b4ca0236e9e35a093c230e0cccfe4a2b
Nazarokkk <0a:a4:51:3b:69:31:f5:74:4a:31:4c:8a:7f:7c:4e:58>
2015-07-12 12:25:37 +0300

    add webfragment

--- app/src/main/java/com/example/nazarkorchak/first/MainActivity.java
+++ app/src/main/java/com/example/nazarkorchak/first/MainActivity.java
@@ -1,9 +1,8 @@
 package com.example.nazarkorchak.first;
 
+import android.app.FragmentManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,27 +11,14 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //add a fragment
+        WebFragment myFragment = new WebFragment();
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.FragmentContainer, myFragment).commit();
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
