4a20c978e49f072c2418b1c281bf791eae70e4e2
Nazarokkk <0a:a4:51:3b:69:31:f5:74:4a:31:4c:8a:7f:7c:4e:58>
2015-07-12 13:16:41 +0300

    made can't change orientation

--- app/src/main/java/com/example/nazarkorchak/first/WebFragment.java
+++ app/src/main/java/com/example/nazarkorchak/first/WebFragment.java
@@ -5,14 +5,32 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class WebFragment extends Fragment {
+
+    String mURL = "https://oauth.vk.com/authorize?client_id=4980525&redirect_uri=https://oauth.vk.com/blank.html&scope=friends,video,offline&revoke=1&display=mobile";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.webfragment, container, false);
+
+        View view = (View)inflater.inflate(R.layout.webfragment, container, false);
+
+        WebView myWebView = (WebView) view.findViewById(R.id.webView);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.setWebViewClient(new MyWebViewClient());
+        myWebView.loadUrl(mURL.trim());
+
+        return view;
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            return false;
+        }
     }
 
 }
