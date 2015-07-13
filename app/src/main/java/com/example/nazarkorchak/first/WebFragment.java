cd84ea16e9fcb29b29d4882ad3c01fa49f3bcf08
Nazarokkk <0a:a4:51:3b:69:31:f5:74:4a:31:4c:8a:7f:7c:4e:58>
2015-07-13 12:34:44 +0300

    create editor and preferences

--- app/src/main/java/com/example/nazarkorchak/first/WebFragment.java
+++ app/src/main/java/com/example/nazarkorchak/first/WebFragment.java
@@ -1,7 +1,11 @@
 package com.example.nazarkorchak.first;
 
 import android.app.Fragment;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,7 +14,9 @@ import android.webkit.WebViewClient;
 
 public class WebFragment extends Fragment {
 
-    String mURL = "https://oauth.vk.com/authorize?client_id=4980525&redirect_uri=https://oauth.vk.com/blank.html&scope=friends,video,offline&revoke=1&display=mobile";
+    private static final String TAG = "WebFragment";
+    String mURL = "https://oauth.vk.com/authorize?client_id=4980525&redirect_uri=https://oauth.vk.com/blank.html&scope=friends,video,offline&revoke=1&display=mobile&response_type=token ";
+    String REDIRECT_URI = "access_token";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -23,12 +29,46 @@ public class WebFragment extends Fragment {
         myWebView.setWebViewClient(new MyWebViewClient());
         myWebView.loadUrl(mURL.trim());
 
+
         return view;
     }
 
     private class MyWebViewClient extends WebViewClient {
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
+            Log.d(TAG,url);
+
+            if(url.contains(REDIRECT_URI)) {
+
+                url = url.replace("#","?");
+
+                Uri token_uri = Uri.parse(url);
+
+
+                String accessToken = token_uri.getQueryParameter("access_token");
+                String expiresIn = token_uri.getQueryParameter("expires_in");
+                String userId = token_uri.getQueryParameter("user_id");
+
+                Log.d(TAG, "TOKEN = " + accessToken);
+                Log.d(TAG, "TIME = " + expiresIn);
+                Log.d(TAG, "ID = " + userId);
+
+
+                SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPref.edit();
+               // editor.putString("access_token",accessToken);
+                editor.putString("expires_in",expiresIn);
+                editor.putString("user_id",userId);
+                editor.commit();
+
+                Log.d("EDITOR", "TOKEN_IN_EDITOR = " + sharedPref.getString("access_token", "nothing"));
+                Log.d("EDITOR", "TIME_IN_EDITOR = " + sharedPref.getString("expires_in","nothing"));
+                Log.d("EDITOR", "ID_IN_EDITOR = " + sharedPref.getString("user_id","nothing"));
+
+                return  true;
+            }
+
             return false;
         }
     }
